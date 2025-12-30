95bc402b3e4df615d0b64bceed4e1efb0a4945d5
ss1ngh <singhshreyansh2354@gmail.com>
2025-12-30 05:54:33 +0530

    added routing

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { InfoController } = require('../../controllers');
+const airplaneRoutes = require('./airplane-routes');
 
 router.get('/info', InfoController.info);
+
+router.use('/airplanes', airplaneRoutes);
 
 module.exports = router;
